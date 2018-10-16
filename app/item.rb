0e920be2ee5fa5fbda4fb58c163ad324c4f1ff53
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-10-16 19:43:01 +0000

    automatically backed up by learn

--- app/item.rb
+++ app/item.rb
@@ -2,7 +2,7 @@ class Item
   attr_accessor :name, :price
   @@items = [Item.new("Figs", 9.99), Item.new("Apple", 8.99)]
 
-  def initialize(name,price)
+  def initialize(name, price)
     @name = name
     @price = price
     
